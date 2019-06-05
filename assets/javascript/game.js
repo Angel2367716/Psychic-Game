bff8deb5dea6bb21b4c113976c9b83563654a92d
Marco Angel <angel2367716@gmail.com>
2019-06-04 23:24:20 -0700

    Game can now keep score of wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,36 +3,41 @@ let lossesScore = 0;
 let guessesLeft = 10;
 let guessedLetters = [];
 let computerChoice = ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+//resetGame();
+//display();
 document.onkeyup = function (event) {
     //function getComputerChoice(){
     const userGuess = event.key;
     guessedLetters.push(userGuess);
-    
+
     const computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
 
     if ((userGuess === computerGuess)) {
         //|| (userGuess === "b") || (userGuess === "c")) {
 
         winsScore++;
-        guessesLeft=12;
-    } else if ((userGuess !== computerGuess)){
-        lossesScore++;
+        guessesLeft = 9;
+    }else {
         guessesLeft--;
-    
+    } if (guessesLeft === 0) {
+        lossesScore++;
+        guessesLeft = 10;
+
         //if ((userGuess === computerGuess)) { winsScore++ }
 
         //else if ((userGuess !== computerChoice)) { lossesScore++ }
 
-    }  
-        let html =
-          "<p>Your Guesses so far: " + userGuess + "</p>" +
-        "<p>wins: " + winsScore + "</p>" +
-        "<p>losses: " + lossesScore + "</p>";
-        document.querySelector("#game").innerHTML = html;
-     
-        //var html = "<p>Guess what letter I'm thinking of:</p>" + "<p>Wins: " + youWin + "</p>" + "<p>Loses: "+ youLose + "<p>Guesses Remaining: " + guessesLeft + "<p>Guessed Letter: " +  guessedLetters.join(', '); //Show guessed letters here
-        //document.querySelector("#psychicAct").innerHTML = html;
+    }
+    let html =
+        "<p>Guess what letter I'm thinking of:</p> " +
+        "<p>Wins: " + winsScore + "</p>" +
+        "<p>Losses: " + lossesScore + "</p>" +
+        "<p>Guesses Left: " + guessesLeft + "</p>"
+        "<p>Your Guesses so far: " + guessedLetters.join(', ');
+    document.querySelector("#game").innerHTML = html;
+
+    //var html = "<p>Guess what letter I'm thinking of:</p>" + "<p>Wins: " + youWin + "</p>" + "<p>Loses: "+ youLose + "<p>Guesses Remaining: " + guessesLeft + "<p>Guessed Letter: " +  guessedLetters.join(', '); //Show guessed letters here
+    //document.querySelector("#psychicAct").innerHTML = html;
 
     // if ((userGuess === "a") || (userGuess === "b") ||  {
     //    if ((userGuess === "computerChoice")) {lossesScore++; }
@@ -43,10 +48,10 @@ document.onkeyup = function (event) {
     // winsScore++;
 
 
- 
-       
 
-    
+
+
+
 
 }
 
